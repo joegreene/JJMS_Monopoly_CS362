c27fcc3b642d49cf5572cd400f662ab8fb494230
jedmonds91011 <jedmonds91011@yahoo.com>
2015-02-11 11:12:59 -0800

    Created a unity project
    Added some script and tiles to the scene.

---
+++ JJMS_Monopoly_CS362/Assets/Scripts/GameTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class GameTile : MonoBehaviour {
+
+	public string tileName;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
---
+++ JJMS_Monopoly_CS362/Assets/Scripts/PropertyTile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropertyTile : GameTile {
+
+	public int propertyCost;
+	public int numHouses;
+	public bool hasHotel;
+	public int rentAmount;
+	public Player owner;
+
+	// Use this for initialization
+	void Start () 
+	{
+		owner = null;
+		propertyCost = 0;
+		numHouses = 0;
+		hasHotel = false; 
+		rentAmount = 0; 
+	}
+
+
+
+}
