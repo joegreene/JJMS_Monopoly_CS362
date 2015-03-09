41ba4cfcf1f8fa5c5f9e05cb4d89e849fbe22b1f
mollybracken10 <mollybracken10@Mollys-MacBook-Pro.local>
2015-03-09 11:54:21 -0700

    GUI
    working on guy

---
+++ JJMS_Monopoly_CS362/Assets/Scripts/GUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI : MonoBehaviour {
+	public string stringToEdit ;
+	public int number;
+	//public int number = Random.Range (2,12);
+	private void Start()
+	{
+		number = Random.Range (2, 12);
+		stringToEdit = "0";
+		}
+	private void Update()
+	{
+		}
+		void OnGUI () {
+		stringToEdit = UnityEngine.GUI.TextField(new Rect(5, 5, 20, 20), stringToEdit, 5);
+
+		if (UnityEngine.GUI.Button (new Rect (15, 15, 100, 50), "Roll Dice")) 
+		{
+			stringToEdit = number.ToString ();
+		}
+		if (UnityEngine.GUI.Button (new Rect (15, 100, 120, 50), "Check Bank")) 
+		{
+			transform.position += new Vector3(0, -0.1f, 0);
+		}
+	}
+}
+
+
---
+++ JJMS_Monopoly_CS362/Assets/TestButtonClick.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TestButtonClick : MonoBehaviour {
+	public GUIText buttonText;
+	public string numberString;
+	public bool isClicked = false;
+	public int number;
+	// Use this for initialization
+	public void ClickTest() {
+		Debug.Log ("clicked");
+		isClicked = true;
+		//buttonText.text = getRandomNumber().ToString();
+
+
+	}
+	
+	public void ClickTest2(string text) {
+
+		Debug.Log (text);
+		isClicked = true;
+		buttonText.text = getRandomNumber().ToString();
+
+	}
+	void Start()
+	{
+		//number = Random.Range(2, 13);
+	}
+
+	public int getRandomNumber()
+	{
+		number = Random.Range(2, 13);
+		return number;
+	}
+	void Update()
+	{
+		if (isClicked == true) 
+		{
+			buttonText.text = getRandomNumber().ToString();
+		}
+	}
+
+}
