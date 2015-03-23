008b2906d289ddab8b2bb8c4e9796c40634d3d3e
mollybracken10 <mollybracken10@Mollys-MacBook-Pro.local>
2015-03-23 10:10:12 -0700

    card and GUI

---
+++ JJMS_Monopoly_CS362/Assets/Scripts/Card.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+	public int currentMoney;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void payBank()
+	{
+
+	}
+
+	void receiveFromBank()
+	{
+
+	}
+
+	void payPlayers()
+	{
+	}
+
+	void receiveFromPlayers()
+	{
+
+	}
+
+	void payOnePlayer()
+	{
+
+	}
+
+	void holdOntoGetOutJailCard()
+	{
+
+	}
+
+}
+
+
+/*
+ * using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+	public string playerName;
+	public int playerIndex;
+	public int currentMoney;
+	public int gameBoardIndex;
+
+	private List<GameTile> tilesOwned;
+
+
+	// Use this for initialization
+	void Awake () 
+	{
+		tilesOwned = new List<GameTile>();
+		currentMoney = 1000;
+		playerIndex = GameManager.instance.players.Count - 1;
+		gameBoardIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//checks to see if a specific tile is owned by a player,
+	public bool playerOwnsProperty(GameTile tileToCheck)
+	{
+		foreach(GameTile tile in tilesOwned)
+		{
+			if(tile == tileToCheck)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
+*/
---
+++ JJMS_Monopoly_CS362/Assets/Scripts/ChanceTile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChanceTile : GameTile {
+
+	//Call chance deck to get card
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
